b811f1c241597f66d635f3ec4a4b9679384ab09f
Bennett B Madavana <34763279+bennett1412@users.noreply.github.com>
2023-02-03 00:30:19 +0530

    Fixed typo in name

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import '../styles/blobz.min.css';
 
 export default function MyApp({ Component, pageProps }) {
