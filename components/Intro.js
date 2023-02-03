17418406ebab133374d6bda3688dea8fc44506dd
Bennett B Madavana <34763279+bennett1412@users.noreply.github.com>
2023-02-03 09:43:24 +0530

    Updated font for intro title

--- components/Intro.js
+++ components/Intro.js
@@ -11,7 +11,7 @@ const Intro = () => {
                     fontWeight: '400',
                     fontSize: '50px',
                     lineHeight: '60px',
-                }} className='text-[#8bb82d] text-5xl text-center Varela'>
+                }} className='text-[#8bb82d] text-5xl text-center font-Varela'>
                     Hey, I&apos;m Bennett
                 </div>
                 <div className='w-40 h-50 grid items-center'>
